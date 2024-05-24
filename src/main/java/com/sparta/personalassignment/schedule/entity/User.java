91ebfb493d0accfba67c929e801a0754091100c7
Ham-Seung-Wan <syham001@naver.com>
2024-05-24 20:56:57 +0900

    유저엔티티 제한사항 추가

--- src/main/java/com/sparta/personalassignment/schedule/entity/User.java
+++ src/main/java/com/sparta/personalassignment/schedule/entity/User.java
@@ -1,6 +1,8 @@
 package com.sparta.personalassignment.schedule.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,9 +22,13 @@ public class User extends Timestamped {
     private String nickname;
 
     @Column(name = "username",nullable = false, unique = true)
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "아이디는 영어 소문자와 숫자만 사용하여 4~10자리여야 합니다.")
     private String username;
 
     @Column(name = "password",nullable = false)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "대소문자와 숫자만 입력 가능합니다.")
+    @Size(min = 8, max = 15, message = "길이는 8에서 15 사이여야 합니다.")
     private String password;
 
     @Column(name = "role",nullable = false)
